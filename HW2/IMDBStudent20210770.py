71e17678fa0aec764d53eabf0291cbd62aa81c07
kejjin0 <80269953+kejjin0@users.noreply.github.com>
2023-11-05 07:04:48 +0900

    Update IMDBStudent20210770.py

--- HW2/IMDBStudent20210770.py
+++ HW2/IMDBStudent20210770.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import csv
 
 inputFile = str(sys.argv[1])
 outputFile = str(sys.argv[2])
@@ -21,13 +20,10 @@ with open(inputFile, "rt") as f:
 			else:
 				count[g] = count[g] + 1
 
-with open(outputFile, "wt") as openf:
-	wr = csv.writer(openf)
-	wr.writerow(count.items())
-	#wr.writerow(count)
-
+with open(outputFile, "wt") as writeF:
 	movieKey = count.keys()
 
 	for key in movieKey:
-		print("%s %s" %(key, count[key]), end='\n')
-		#wr.writerow(key + ' ' + str(count[key]))
+		#print("%s %s" %(key, count[key]), end='\n')
+		string = key + " " + str(count[key]) + "\n"
+		writeF.write(string)
